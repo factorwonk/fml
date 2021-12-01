7bd8107a5de5c9dda11d7d583f3928490865ef85
Abhishek Das <factorwonk@protonmail.com>
2021-12-01 07:49:29 +0530

    added 20 day adv code

--- coinbase_price_analysis.py
+++ coinbase_price_analysis.py
@@ -5,8 +5,6 @@ import matplotlib.pyplot as plt
 
 from coinbase_data.coinbase_extract_script import fetch_daily_data
 
-# from statsmodels.tsa.seasonal import seasonal_decompose, STL
-
 
 def import_crypto_ts(symbol):
     path = "//Users//hyperion//Wasteland//Python//Repos//fml//coinbase_data"
@@ -18,10 +16,11 @@ def import_crypto_ts(symbol):
     # df = pd.read_csv("Coinbase_BTCUSD_dailydata.csv", parse_dates=["date"])
     # select columns of interest
     df = df[["date", "open", "close", "high", "low", "volume"]]
+    df = df.set_index("date")
     return df
 
 
-def calc_return(input_df):
+def calc_returns_vols(input_df):
     output_df = input_df
     # Calculate daily returns from close data
     output_df["close_return"] = output_df.close.pct_change(1)
@@ -29,22 +28,32 @@ def calc_return(input_df):
     output_df["close_log_return"] = np.log(output_df.close) - np.log(
         output_df.close.shift(1)
     )
+    # Calculate 20 day average daily volume
+    output_df["20_day_adv"] = output_df.volume.rolling(20).sum() / 20.0
     return output_df
+
+
+def chart_price_vols(input_df):
+    fig, (ax1, ax2) = plt.subplots(2, figsize=(10, 8), sharex=True)
+    fig.suptitle("Price and Volume Charts")
+    ax1.plot(a["date"], a["close"], label="Close Price")
+    ax2.plot(a["date"], a["volume"], label="Volume")
+    plt.show()
+    plt.legend("Close Price History")
+    return 0
 
 
 if __name__ == "__main__":
     # we set which pair we want to retrieve data for
     print("\n")
-    print("Enter the Crypto symbol you want price history for e.g. BTC/USD")
+    print(
+        "Enter the Crypto symbol you want price and volume history for e.g. BTC/USD, ETH/EUR etc"
+    )
     print("\n")
     crypto_symbol = input()
     # a = import_crypto_ts("BTC/USD")
     print("Fetching...")
-    a = import_crypto_ts(crypto_symbol)
-    b = calc_return(a)
-    print(b.tail())
-    b.plot.line(x="date", y="close")
-    plt.show()
-    plt.legend("Close Price History")
+    a = calc_returns_vols(import_crypto_ts(crypto_symbol))
+    print(a.tail(10))
     print("\n")
     print("done...")
