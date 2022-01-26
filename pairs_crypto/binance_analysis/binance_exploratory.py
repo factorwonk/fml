ee40f14ce44b11522de886727d2647fc186ebcb8
Abhishek Das <factorwonk@protonmail.com>
2022-01-26 21:28:06 +0530

    added price and vol charts for binance pair

--- pairs_crypto/binance_analysis/binance_exploratory.py
+++ pairs_crypto/binance_analysis/binance_exploratory.py
@@ -1,8 +1,12 @@
 import os
 import pandas as pd
 
+import matplotlib.pyplot as plt
 
-def load_binance_data():
+from datetime import datetime
+
+
+def load_1min_binance_data():
     # display all columns in output
     pd.set_option("display.max_columns", None)
     # Hardcode path for binance data
@@ -10,14 +14,32 @@ def load_binance_data():
         "//Users//hyperion//Wasteland//Python//Repos//fml//pairs_crypto//binance_data//"
     )
     df = pd.read_parquet(os.path.join(path, "BTC-USDT.parquet"))
-    select_cols = ["close", "volume", "number_of_trades"]
-    df = df[select_cols]
+    # Filter down to columns of interest
+    # Index is open time, hence use open prices
+    select_cols = ["open", "volume", "number_of_trades"]
+    df = df[select_cols].reset_index()
     return df
 
 
+def chart_1min_binance_price_vols(input_df):
+    # init date
+    date = datetime.now().strftime("%Y%m%d")
+    # init path
+    save_path = "//Users//hyperion//Wasteland//Python//Repos//fml//pairs_crypto//binance_outputs//"
+    fig, (ax1, ax2) = plt.subplots(2, figsize=(10, 8), sharex=True)
+    fig.suptitle("Close Price and Volume Charts")
+    ax1.plot(a["open_time"], a["open"], label="Open Price")
+    ax2.plot(a["open_time"], a["volume"], label="Volume")
+    plt.legend("Price and Volume History")
+    plt.savefig(os.path.join(save_path, f"BTC_USDT_price_vol_{date}.png"))
+    plt.close(fig)
+
+
 if __name__ == "__main__":
-    print("Start loading Binance Data\n")
+    print("Start loading 1 minute Binance Data\n")
     print("\nBTC_USDT pair\n")
-    a = load_binance_data()
+    a = load_1min_binance_data()
     print(a)
+    print("\nCharting the price and volume for BTC_USDT\n")
+    chart_1min_binance_price_vols(a)
     print("\nEnd\n")
