913eb32aaa6e689d1a6a659aee5a2749f04e64b5
Abhishek Das <factorwonk@protonmail.com>
2022-04-04 10:56:54 +0530

    added code to fit spread to or-uh process

---
+++ pairs_crypto/binance_analysis/binance_fit_spread.py
@@ -0,0 +1,12 @@
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+from datetime import datetime
+from scipy.stats import iqr
+
+
+def fit_spread_oruh(residuals):
+    
